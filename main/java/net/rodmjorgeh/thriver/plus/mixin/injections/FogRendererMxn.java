387185129f931c509fc3e535e0a46e62ca20c4da
Rodmjorgeh <rodmjorge7@gmail.com>
2025-05-27 11:48:21 +0100

    finally finished everything about the doll's eye fog animation

--- main/java/net/rodmjorgeh/thriver/plus/mixin/injections/FogRendererMxn.java
+++ main/java/net/rodmjorgeh/thriver/plus/mixin/injections/FogRendererMxn.java
@@ -2,6 +2,7 @@ package net.rodmjorgeh.thriver.plus.mixin.injections;
 
 import com.mojang.blaze3d.shaders.FogShape;
 import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.FogParameters;
 import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.util.Mth;
@@ -13,6 +14,7 @@ import net.rodmjorgeh.thriver.plus.EntityAdd;
 import net.rodmjorgeh.thriver.world.area.block.DollsEyesBlock;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,39 +23,51 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(FogRenderer.class)
 public class FogRendererMxn {
 
+    @Unique private static int FOG_CHANGE_TICK = 10;
+
+    @Inject(method = "computeFogColor", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private static void computeFogColor(Camera camera, float partialTick, ClientLevel level, int renderDistance, float darkenWorldAmount,
+                                        CallbackInfoReturnable<Vector4f> cir, FogType fogType, Entity entity, float r, float g, float b) {
+        if (entity instanceof EntityAdd aEntity) {
+            int i = aEntity.getTicksToBlind();
+            if (i > 0) {
+                float fogColorMax = (FOG_CHANGE_TICK - i) / (float)FOG_CHANGE_TICK;
+                r = r * Math.max(0.0F, fogColorMax);
+                g = g * Math.max(0.0F, fogColorMax);
+                b = b * Math.max(0.0F, fogColorMax);
+
+                cir.setReturnValue(ClientHooks.getFogColor(camera, partialTick, level, renderDistance, darkenWorldAmount, r, g, b));
+            }
+        }
+    }
+
     @Inject(method = "setupFog", at = @At(value = "RETURN", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
     private static void setupFog(Camera camera, FogRenderer.FogMode fogMode, Vector4f fogColor, float renderDistance, boolean isFoggy, float partialTick,
                                  CallbackInfoReturnable<FogParameters> cir, FogType fogType, Entity entity, FogRenderer.FogData fogData) {
         if (entity instanceof EntityAdd aEntity) {
             int i = aEntity.getTicksToBlind();
-            if (aEntity.getIsInDollsEyes() && i > 0) {
-                final int j = 10;
-                final double maxTick = DollsEyesBlock.TICKS_TO_BLIND;
+            if (i > 0) {
 
-                ThriverMod.LOGGER.debug("Starting End: " + fogData.end);
-                ThriverMod.LOGGER.debug("Tick: " + i);
+                final double minEnd = 5.0F; // The fog end for the blindness effect
+                final double maxTick = DollsEyesBlock.TICKS_TO_BLIND;
                 final double distance = fogData.end;
 
+                /* Special thanks to Desmos (yes, the online calculator) for graph visualization, so I can see what
+                   equations need to be done in order for this to be smooth.
+
+                   You can check the 2 equations from "end" and "end1" by replacing distance for the average render
+                   distance, which gives a value of 192, and maxTick for 100. Then, have x = i.*/
                 double end = (distance * (maxTick / 100.0F)) / Math.pow(i, 0.35F);
-                double end1 = (Math.pow(maxTick - i, 2.0F) * distance) / (200.0F * maxTick) + 5.0F;
-                double end2 = Mth.lerp(i / maxTick, end, end1);
-                double end3 = (i > j) ? end1 : end2;
+                double end1 = (Math.pow(maxTick - i, 2.0F) * distance) / (200.0F * maxTick) + minEnd;
+                double end2 = Mth.lerp((float)i / FOG_CHANGE_TICK, end, end1);
+                double end3 = (i > FOG_CHANGE_TICK) ? end1 : end2;
 
                 fogData.start = 0.0F;
-                fogData.end = (float)Math.max(end3, 5.0F);
+                fogData.end = (float)Math.max(end3, minEnd);
                 fogData.shape = FogShape.SPHERE;
 
-                float fogColorMax = (j - i) / (float)j;
-                float r = fogColor.x * Math.max(0.0F, fogColorMax);
-                float g = fogColor.y * Math.max(0.0F, fogColorMax);
-                float b = fogColor.z * Math.max(0.0F, fogColorMax);
-                float w = fogColor.w;
-
-                ThriverMod.LOGGER.debug("Start: " + fogData.start);
-                ThriverMod.LOGGER.debug("End: " + fogData.end);
-
                 cir.setReturnValue(ClientHooks.onFogRender(fogMode, fogType, camera, partialTick, renderDistance,
-                        new FogParameters(fogData.start, fogData.end, fogData.shape, r, g, b, w)));
+                        new FogParameters(fogData.start, fogData.end, fogData.shape, fogColor.x, fogColor.y, fogColor.z, fogColor.w)));
             }
         }
     }
